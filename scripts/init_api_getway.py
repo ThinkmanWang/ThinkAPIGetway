9cc990f1f9a2ff38c4938871c29eb0a40a1c8fb9
Thinkman Wang <wangxf1985@gmail.com>
2021-01-12 22:58:47 +0800

    update

--- scripts/init_api_getway.py
+++ scripts/init_api_getway.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
 import requests
 import json
 import jwt
