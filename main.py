eb3e876f43fc009b8cf2b2662f28fc37ba5a941c
Thinkman Wang <wangxf1985@gmail.com>
2021-01-13 21:58:17 +0800

    update

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def sync_apigetway():
 
 async def on_server_started():
     g_logger.info("Server Started!!!")
-    tornado.httpclient.AsyncHTTPClient.configure(None, max_clients=10240)
+    tornado.httpclient.AsyncHTTPClient.configure(None, max_clients=102400)
     # tornado.httpclient.AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
 
     await MainHandler.init_api_getway()
