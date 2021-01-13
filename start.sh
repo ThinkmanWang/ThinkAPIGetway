58c4525e073ad5c13b3c5bcacec41dedf79b1e54
Thinkman Wang <wangxf1985@gmail.com>
2021-01-13 13:39:26 +0800

    update

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 cd /root/Github-Thinkman/ThinkAPIGetway
 export PATH="$PATH:/opt/bin"
 
+pipenv run python scripts/init_api_getway.py
 pipenv run python main.py -Dpname=ThinkAPIGetway > /dev/null 2>&1 &
 
