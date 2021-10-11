d76f0243e937b497c8ee2e1611524c957d0cbbaf
Thinkman Wang <wangxf1985@gmail.com>
2021-10-11 13:56:44 +0800

    update

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 cd /root/Github-Thinkman/ThinkAPIGetway
 export PATH="$PATH:/opt/bin"
+export PATH="$PATH:/usr/local/bin"
 
 pipenv run python scripts/init_api_getway.py
 pipenv run python main.py -Dpname=ThinkAPIGetway > /dev/null 2>&1 &
--- start_back_redis.sh
+++ start_back_redis.sh
@@ -2,5 +2,6 @@
 
 cd /root/Github-Thinkman/ThinkAPIGetway
 export PATH="$PATH:/opt/bin"
+export PATH="$PATH:/usr/local/bin"
 
-pipenv run python scripts/backup_redis.py
+pipenv run python scripts/backup_redis.py -Dpname=backup_redis > /dev/null 2>&1 &
